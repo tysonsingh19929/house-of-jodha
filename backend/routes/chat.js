395d327458f84afab487d3e8c5957d83eada03ef
tyson singh <nikhilsingh19929@gmail.com>
2026-04-18 18:32:35 +0530

    update

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -113,12 +113,14 @@ router.post('/message', async (req, res) => {
         query.name = { $regex: new RegExp(matchedColor, 'i') };
       }
 
-      // Fetch up to 5 matching products, or just 5 random products if no match
-      let products = await Product.find(query).limit(5);
+      // Fetch up to 5 matching products, but ONLY grab name, price, and ID. No heavy images!
+      let products = await Product.find(query).select('name price _id').limit(5);
       
-      // If we didn't find enough, fetch some random ones
+      // If we didn't find enough, fetch some random ones (again, only small data)
       if (products.length < 5) {
-          const additionalProducts = await Product.find({ _id: { $nin: products.map(p => p._id) } }).limit(5 - products.length);
+          const additionalProducts = await Product.find({ _id: { $nin: products.map(p => p._id) } })
+              .select('name price _id')
+              .limit(5 - products.length);
           products = [...products, ...additionalProducts];
       }
 
